afd75930b84cd385f320f01810e6c15326bd8167
himanshu86 <himanshu_rastogi86@yahoo.co.in>
2014-05-19 12:18:20 +0530

    prog for computing inverse of matrix
    the program has two functions taking advantage of lexical scoping to get
    already computed result from cache

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
+#The below functions fetch the inverse from cache if the result has already been computed
+#previously else it computes and displays the result
 
-## Write a short comment describing this function
+#This function returns a list of functions for setting and getting data for the matrix
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix<-function(x=matrix())
+{
+    i <- NULL
+    set <- function(y) {
+        x <<- y
+        i <<- NULL
+    }
+    get <- function() x
+    setinv <- function(inv) i <<- inv
+    getinv <- function() i
+    list(set = set, get = get,
+         setinv = setinv,
+         getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+#This function uses the functions of "makeCacheMatrix" to get the inverse of a matrix
+#If the inverse is being computed for the first time, it computes and displays the result
+#Else it gets the result from cache and displays
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve<-function(y)
+{
+    i <- y$getinv()
+    if(!is.null(i)) {
+        message("getting cached data")
+        return(i)
+    }
+    data <- y$get()
+    i <- solve(data)
+    y$setinv(i)
+    i
 }
